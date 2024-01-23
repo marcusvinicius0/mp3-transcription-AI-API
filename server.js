38ad362435a308d3311f2767ff19e7a035f4efad
unknown <begheli2020@gmail.com>
2024-01-23 16:51:33 -0300

    chore: vercel settings

--- server.js
+++ server.js
@@ -2,5 +2,5 @@
 import app from "./app.js";
 const PORT = 3000;
 
-app.listen(PORT || 3001, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT || 9001, () => console.log(`Listening on port ${PORT}`));
 
